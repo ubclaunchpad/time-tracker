6831907d415c63b27844ebccd8ab7f32baa16053
3067190 <runshaunddd@gmail.com>
2020-10-24 12:45:07 -0700

    added capability for hard-coded list with 1 item. Need to add 4 more items and figure out why it is repeating the 1 item to fill the screen

--- lib/screens/task_list.dart
+++ lib/screens/task_list.dart
@@ -1,49 +1,47 @@
 import 'package:flutter/material.dart';
 
 class TaskListScreen extends StatelessWidget {
-
+    
 	@override
 	Widget build(BuildContext context){
 		return Scaffold(
-			appBar: AppBar(
-			   title: Text('Task List'),
-     		 	),
-      			body: Center(
-			   child: new Column(
-          			mainAxisAlignment: MainAxisAlignment.center,
-          			children: <Widget>[
-            				new Text(
-             				 'Task List Screen',
-            				),
-         			],
-			   )
-			)
-    		);
+			appBar: AppBar(title: Text('Task List')),
+      			body: BodyLayout(),
+			);
   	}
 }
-
-abstract class ListItem {
-	Widget buildTitle(BuildContext context);
-	Widget buildSubtitle(BuildContext context);
-}
-
-class HeadingItem implements ListItem{
-	final String heading;
-	HeadingItem(this.heading);
-	Widget buildTitle(BuildContext context){
-	     return Text(
-	          heading,
-                  style: Theme.of(context).textTheme.headline5,
-	);
-     } 
-     Widget buildSubtitle(BuildContext context) => null;
-}
-
-class MessageItem implements ListItem { 
-     final String sender;
-     final String body;
-     MessageItem(this.sender, this.body);
-     Widget buildTitle(BuildContext context) => Text(sender);
-     Widget buildSubtitle(BuildContext context) => Text(body);
-}
 			 
+class BodyLayout extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+		return _myListView(context);
+	}
+}
+
+Widget _myListView(BuildContext context) {
+	Widget column = Expanded(
+		child: Column(
+			crossAxisAlignment: CrossAxisAlignment.start,
+			children: <Widget>[
+				Text('Finish Launch Pad Issue 2',style:TextStyle(fontSize:16),),
+				Text('Extracurriculars'),
+				Text('215 minutes'),
+			],
+		),
+	);
+	return ListView.builder(
+		itemBuilder:(context,index){
+			return Card(
+				child:Padding(
+					padding: const EdgeInsets.all(8.0),
+					child: Row(
+						children: <Widget>[
+							column,
+]
+),
+),
+);
+}
+);
+} 
+
