7dd733f249668109dfa7d4d83379bbb7206bb6a6
3067190 <runshaunddd@gmail.com>
2020-10-24 13:57:31 -0700

    Changed button so now it moves to new screen using list icon instead of center button on home screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,17 @@ class MyApp extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text('Time Tracker'),
+	  actions:[
+		IconButton(icon: Icon(Icons.list),
+	 	onPressed:(){
+			Navigator.push(context,MaterialPageRoute(builder:(context)=>TaskListScreen()),);
+		}),	
+	  ],
         ),
         body: Center(
-	  child: RaisedButton(
-             child: Text('Track your time!'),
-	     color: Theme.of(context).primaryColor,
-	     textColor: Colors.white,
- 	     onPressed: (){
-	        Navigator.push(context, MaterialPageRoute(builder:(context) => TaskListScreen()),
-	     );
-	   }
+          child: Text('Track your time!'),
         ),
-      ),
-   );
-  }
-}
+     );
+    }
+ }
+
