d3a42a197ee17af305bf9f43bf1ef68f062bf4af
3067190 <runshaunddd@gmail.com>
2020-10-24 13:11:33 -0700

    hardcoded list items with 5 descriptions, need to add 2 other fields

--- lib/screens/task_list.dart
+++ lib/screens/task_list.dart
@@ -12,13 +12,15 @@ class TaskListScreen extends StatelessWidget {
 }
 			 
 class BodyLayout extends StatelessWidget {
+	List<String> titles = ['Finish Launch Pad Issue 2','Write blog post for team website',
+			'Clean out closet','In-class exercise CPSC 304','Midterm essay ASIA 311'];
 	@override
 	Widget build(BuildContext context) {
 		return _myListView(context);
 	}
-}
 
-Widget _myListView(BuildContext context) {
+	Widget _myListView(BuildContext context) {
+	/*
 	Widget column = Expanded(
 		child: Column(
 			crossAxisAlignment: CrossAxisAlignment.start,
@@ -26,22 +28,25 @@ Widget _myListView(BuildContext context) {
 				Text('Finish Launch Pad Issue 2',style:TextStyle(fontSize:16),),
 				Text('Extracurriculars'),
 				Text('215 minutes'),
+				Text('Write blog post for team website',style:TextStyle(fontSize:16),),
+                                Text('Triathlon'),
+                                Text('43 minutes'),
+				Text('Reorganize Closet',style:TextStyle(fontSize:16),),
+                                Text('Apartment'),
+                                Text('74 minutes'),
 			],
 		),
-	);
+	);*/
 	return ListView.builder(
+		itemCount: titles.length,
 		itemBuilder:(context,index){
+			final item = titles[index];
 			return Card(
-				child:Padding(
-					padding: const EdgeInsets.all(8.0),
-					child: Row(
-						children: <Widget>[
-							column,
-]
-),
-),
-);
-}
-);
+				child:ListTile(
+					title: Text(item),
+				),
+			);
+		}
+	);
 } 
-
+}
