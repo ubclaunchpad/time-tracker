e6b68fecbce1073a13f50e17d05e6ec5123dc466
3067190 <runshaunddd@gmail.com>
2020-10-24 00:56:27 -0700

    added capability to switch from home screen to task list screen and back

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'screens/task_list.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+    title: "Time Tracker",
+    home: MyApp(),
+));
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Time Tracker',
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text('Time Tracker'),
         ),
         body: Center(
-          child: Text('Track your time!'),
+	  child: RaisedButton(
+             child: Text('Track your time!'),
+	     color: Theme.of(context).primaryColor,
+	     textColor: Colors.white,
+ 	     onPressed: (){
+	        Navigator.push(context, MaterialPageRoute(builder:(context) => TaskListScreen()),
+	     );
+	   }
         ),
       ),
-    );
+   );
   }
 }
