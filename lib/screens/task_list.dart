e6bcb84bf68ba41913158225d452d3c143362040
3067190 <runshaunddd@gmail.com>
2020-10-24 13:32:09 -0700

    hard-coded 5 tasks with description, category, and time taken

--- lib/screens/task_list.dart
+++ lib/screens/task_list.dart
@@ -14,36 +14,22 @@ class TaskListScreen extends StatelessWidget {
 class BodyLayout extends StatelessWidget {
 	List<String> titles = ['Finish Launch Pad Issue 2','Write blog post for team website',
 			'Clean out closet','In-class exercise CPSC 304','Midterm essay ASIA 311'];
+	List<String> subtitles = ['Launch Pad - 215 minutes','Triathlon - 47 minutes','Apartment - 74 minutes',
+					'Assignments - 23 minutes','Assignments - 201 minutes'];
 	@override
 	Widget build(BuildContext context) {
 		return _myListView(context);
 	}
 
 	Widget _myListView(BuildContext context) {
-	/*
-	Widget column = Expanded(
-		child: Column(
-			crossAxisAlignment: CrossAxisAlignment.start,
-			children: <Widget>[
-				Text('Finish Launch Pad Issue 2',style:TextStyle(fontSize:16),),
-				Text('Extracurriculars'),
-				Text('215 minutes'),
-				Text('Write blog post for team website',style:TextStyle(fontSize:16),),
-                                Text('Triathlon'),
-                                Text('43 minutes'),
-				Text('Reorganize Closet',style:TextStyle(fontSize:16),),
-                                Text('Apartment'),
-                                Text('74 minutes'),
-			],
-		),
-	);*/
 	return ListView.builder(
 		itemCount: titles.length,
 		itemBuilder:(context,index){
 			final item = titles[index];
+			final moreInfo = subtitles[index];
 			return Card(
 				child:ListTile(
-					title: Text(item),
+					title: Text(item),subtitle:Text(moreInfo)
 				),
 			);
 		}
