967fbcfcedffa8db0d8e1633bfb4889b70f82f74
Miguel Valarao <miguel.valarao@gmail.com>
2020-11-06 15:27:33 -0800

    Fix test helper

--- test/test_helper.dart
+++ test/test_helper.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class TestHelper {
   static Widget makeTestableWidget(Widget child) {
     return MaterialApp(
-      home: Scaffold(body: child),
+      home: child,
     );
   }
 }
