e107b7da00b02d2a4c8d5d844d681efbace128ff
Sarah Bornais <me@sarahbornais.dev>
2020-11-07 11:18:00 -0800

    Merge master into 3-setup-database

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker/models/task.dart';
 import 'package:time_tracker/database/database_helper.dart';
 import 'dart:developer' as logger;
+import 'package:time_tracker/src/time_tracker.dart';
 
 void main() async {
   var db = new DatabaseHelper();
 
   // final task = Task(id: 0, description: 'Fido', category: 'Work', clock: 2001);
   // await db.insertTask(task);
-  return runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Time Tracker',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Time Tracker'),
-        ),
-        body: Center(
-          child: Text('Track your time!'),
-        ),
-      ),
-    );
-  }
+  return runApp(TimeTracker());
 }
