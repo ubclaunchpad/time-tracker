b9cf8540c6ebffe20ddd8287df311cce545dc9a7
shaunedonnelly@gmail.com <runshaunddd@gmail.com>
2020-11-07 18:49:46 -0800

    Schedule task screen in progress, see screen_shots folder

--- lib/src/screens/schedule_task.dart
+++ lib/src/screens/schedule_task.dart
@@ -5,9 +5,42 @@ import 'package:time_tracker/src/utils/time_util.dart';
 class ScheduleTaskScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Center(
+    // TODO: implement build
+    return Scaffold(
       key: Key('schedule_task_screen'),
-      child: Text('Schedule a task!'),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: 20.0,
+            horizontal: 30.0,
+            ),
+            child: renderTaskScheduling()),)
     );
-    }
+  }
+  
 }
+
+Widget renderTaskScheduling(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget> [
+      SizedBox(height:30),
+      _renderTextField('Task title'),
+      SizedBox(height:20),
+      _renderTextField('Date'),
+      SizedBox(height:20),
+      Row(children: <Widget>[
+        Expanded(child: _renderTextField('Start time'),),
+        Expanded(child: _renderTextField('End time')),
+      ],),
+      SizedBox(height:30),
+      Text('Category'),
+    ],);
+}
+
+Widget _renderTextField(String title) => TextField(
+  decoration: InputDecoration(
+    border: InputBorder.none,
+    labelText: title,
+  ),
+);
