95146c572b26b1ca11a3a5c0ed0fb40bfa17f35b
ShaunDonn <shaunedonnelly@gmail.com>
2020-11-28 17:41:15 -0800

    Modified task_list screen to display tasks in database

--- lib/src/screens/task_list.dart
+++ lib/src/screens/task_list.dart
@@ -1,39 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/src/database/database_helper.dart';
+import '../models/task.dart';
 
-//TODO: Replace "titles" and "subtitles" after creating a Task class
 class TaskListScreen extends StatelessWidget {
-  final List<String> titles = [
-    'Finish Launch Pad Issue 2',
-    'Write blog post for team website',
-    'Clean out closet',
-    'In-class exercise CPSC 304',
-    'Midterm essay ASIA 311'
-  ];
-  final List<String> subtitles = [
-    'Launch Pad - 215 minutes',
-    'Triathlon - 47 minutes',
-    'Apartment - 74 minutes',
-    'Assignments - 23 minutes',
-    'Assignments - 201 minutes'
-  ];
+  Future getTasks() async {
+    List<Task> taskList = await DatabaseHelper.internal().tasks();
+    return taskList;
+  }
+
+  Widget taskListWidget() {
+    return FutureBuilder(
+        future: getTasks(),
+        builder: (context, AsyncSnapshot taskListSnapshot) {
+          if (taskListSnapshot.hasData) {
+            return ListView.builder(
+              itemCount: taskListSnapshot.data.length,
+              itemBuilder: (context, index) {
+                final item = taskListSnapshot.data[index].description;
+                final moreInfo = taskListSnapshot.data[index].category + "\n" 
+                      + (taskListSnapshot.data[index].startTime as String) + " - " 
+                      + (taskListSnapshot.data[index].stopTime as String);
+                return Card(
+                    child: ListTile(
+                      key: Key('list_tile_$index'),
+                      title: Text(item),
+                      subtitle: Text(moreInfo),
+                      isThreeLine: true,
+                  ));
+              },
+            );
+          }else{
+            return Container();
+          }
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: Key('task_list_screen'),
-      body: ListView.builder(
-          itemCount: titles.length,
-          itemBuilder: (context, index) {
-            final item = titles[index];
-            final moreInfo = subtitles[index];
-            return Card(
-              child: ListTile(
-                key: Key('list_tile_$index'),
-                title: Text(item),
-                subtitle: Text(moreInfo),
-              ),
-            );
-          }),
+      body: taskListWidget(),
     );
   }
 }
